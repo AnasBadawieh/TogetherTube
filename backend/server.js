4062bd1acf29b18e64cefbb9c3d97d1e5dfd57fb
Anas <a.badawiah@gmail.com>
2025-01-18 13:06:56 +0200

    MongoDB Connection Success Console log Validation

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ const io = new Server(server);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});.then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error(err);
 });
 
 app.use(express.json());
